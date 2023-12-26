abd4f044da5309326a5c2da7a9f5d0278fdc8bb0
leducanh2410 <57927000+leducanh2410@users.noreply.github.com>
2023-12-26 15:28:15 +0700

    Initial commit

---
+++ src/main/java/com/pmis/payload/response/DashboardResponse.java
@@ -0,0 +1,42 @@
+package com.pmis.payload.response;
+
+import java.util.List;
+
+public class DashboardResponse {
+    private String dashboardId;
+    private String layout;
+    private List<DashboardChartOptionsResponse> dashboardChartOptionsResponses;
+
+    public DashboardResponse(String dashboardId, String layout, List<DashboardChartOptionsResponse> dashboardChartOptionsResponses) {
+        this.dashboardId = dashboardId;
+        this.layout = layout;
+        this.dashboardChartOptionsResponses = dashboardChartOptionsResponses;
+    }
+
+    public DashboardResponse() {
+    }
+
+    public String getDashboardId() {
+        return dashboardId;
+    }
+
+    public void setDashboardId(String dashboardId) {
+        this.dashboardId = dashboardId;
+    }
+
+    public String getLayout() {
+        return layout;
+    }
+
+    public void setLayout(String layout) {
+        this.layout = layout;
+    }
+
+    public List<DashboardChartOptionsResponse> getDashboardChartOptionsResponses() {
+        return dashboardChartOptionsResponses;
+    }
+
+    public void setDashboardChartOptionsResponses(List<DashboardChartOptionsResponse> dashboardChartOptionsResponses) {
+        this.dashboardChartOptionsResponses = dashboardChartOptionsResponses;
+    }
+}
